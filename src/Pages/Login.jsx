c5a79201f44558322f1cbaac51a3af1390d0b0e4
mehedihasan770 <mehedihasantrbd@gmail.com>
2026-02-26 16:51:57 +0600

    feat: setup react router with AuthLayout and integrate Tailwind CSS

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className=''>
+            hallo log in now
+        </div>
+    );
+};
+
+export default Login;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,1 +1,16 @@
-export const router = 
+import { createBrowserRouter } from "react-router";
+import AuthLayout from "../Layouts/AuthLayout";
+import Login from "../Pages/Login";
+
+export const router = createBrowserRouter([
+    {
+        path : "/",
+        Component : AuthLayout,
+        children : [
+            {
+                index : true,
+                Component : Login
+            }
+        ]
+    }
+])
